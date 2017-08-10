008c7fb7290033bc45938355d3ff6acf7a4b4377
loloxiaoz <871514048@qq.com>
2017-08-10 14:02:13 +0800

    ok

---
+++ include/houghline.h
@@ -0,0 +1,22 @@
+#ifndef HOUGHLINE_H
+#define HOUGHLINE_H
+
+typedef struct
+{
+    int x;
+    int y;
+}pointInt;
+
+typedef struct
+{
+    float x;
+    float y;
+}pointFloat;
+
+class HoughLine
+{
+public:
+    HoughLine();
+};
+
+#endif // HOUGHLINE_H
